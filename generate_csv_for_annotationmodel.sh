956713061b051adcbf577ea2489468d73aabebff
kurzum <kurzum@googlemail.com>
2013-02-07 11:48:46 +0000

    split script in two

---
+++ generate_csv_for_annotationmodel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TARGETFOLDER=olia.nlp2rdf.org/owl
+
+transform_annotation_model()
+{
+        rapper -i rdfxml $1  | \
+        grep 'http://purl.org/olia/system.owl#hasTag' | \
+        grep -v "DatatypeProperty" | \
+        sed 's/\^\^<http:\/\/www.w3.org\/2001\/XMLSchema#string>//' | \
+        cut -f1,3 -d '>' | \
+        sed 's/<//;s/> "/\t/;s/" .//' | \
+        awk 'BEGIN {FS=OFS="\t"}{t=$1;$1=$2;$2=t;print}'
+}
+
+for FILE in `find $TARGETFOLDER -name "*.owl" `
+do
+        transform_annotation_model $FILE > "$FILE"".csv"
+done
+
