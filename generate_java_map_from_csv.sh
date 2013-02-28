11327975061763aa51d9c2337e496e1cdad9886e
kurzum <kurzum@googlemail.com>
2013-02-28 18:55:34 +0000

    java generation script finished

--- generate_java_map_from_csv.sh
+++ generate_java_map_from_csv.sh
@@ -11,9 +11,22 @@ import java.util.HashMap;
 public class OLiAMap { 
 "
 
+TMP_DIR=`mktemp -d`
+
+
 for FILE in `find $DIR -name "*.csv" `
 do
-	MAPNAME=$(echo "$FILE" | sed 's/.owl.csv$//;s/.rdf.csv//;s/.*\///;s/-/_/g;s/[0-9]/INT/g' )
+	if [ -s $FILE ]
+	then
+		cp $FILE $TMP_DIR
+	fi
+done
+
+for FILE in `ls $TMP_DIR `
+do
+	
+	FILE="$TMP_DIR""/""$FILE"
+	MAPNAME=$(echo "$FILE" | sed 's/.csv$//;s/.csv//;s/.*\///;s/-/_/g;s/\./_/;s/0/a/g;s/1/b/g;s/2/c/g;s/3/d/g;s/4/e/g;s/5/f/g;s/6/g/g;s/7/h/g;s/8/i/g;s/9/j/g;' )
 	echo "	public static MultiValueMap $MAPNAME = MultiValueMap.decorate(new HashMap<String, ArrayList<String>>());
 	
 	static {
