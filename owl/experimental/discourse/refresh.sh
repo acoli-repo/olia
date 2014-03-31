72896586829f47f8b17dfae3b1f32a8ce98ec052
chiarcos <christian.chiarcos@web.de>
2014-03-31 10:58:53 +0000

    olia_discourse docu and update script

---
+++ owl/experimental/discourse/refresh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# update content
+svn update svn://svn.code.sf.net/p/olia/code/trunk/owl/experimental/discourse discrel; 
+svn update svn://svn.code.sf.net/p/olia/code/trunk/owl/experimental/is is; 
+chmod -R a+r .;
+
+# update symlinks
+for file in */*; do
+	if [ -L $file ] ; then
+		if [ -e $file ] ; then echo ok >&/dev/null; 
+		else rm -f $file; fi;
+	fi;
+done
+for file in */*.owl */*.rdf; do 
+	tgt=`echo $file | sed s/'.*\/'//`;
+	if [ -e $TGT/$tgt ] ; then echo ok >&/dev/null; 
+	else ln -s $file $tgt; chmod a+r $tgt; 
+	fi;
+done;
